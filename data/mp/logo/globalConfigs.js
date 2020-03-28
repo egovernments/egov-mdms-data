aa5f12bfa50ce1559d067616fd947474b3a30e1a
Abhilashegov <abhilash.seth@egovernments.org>
2020-03-28 22:48:46 +0530

    adding globalConfig file

---
+++ data/mp/logo/globalConfigs.js
@@ -0,0 +1,15 @@
+var globalConfigs = (function() {
+  var stateTenantId = 'pb'
+  var gmaps_api_key = 'AIzaSyAQOd09-vjmk1sXFb_ZQYDz2nlfhXq7Wf8'
+  var getConfig = function(key) {
+      if(key === 'STATE_LEVEL_TENANT_ID'){
+          return stateTenantId;
+      } 
+      else if(key === 'GMAPS_API_KEY'){
+        return gmaps_api_key;
+      }
+  };
+  return {
+      getConfig
+  };
+}());
