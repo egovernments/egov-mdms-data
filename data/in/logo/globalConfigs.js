eef4c29bd7cb55e0b3263aef29b1fca6e7615d22
Nithin <36623418+nithindv@users.noreply.github.com>
2020-03-30 13:35:08 +0530

    Update globalConfigs.js

--- data/in/logo/globalConfigs.js
+++ data/in/logo/globalConfigs.js
@@ -1,15 +1,18 @@
-var globalConfigs = (function() {
-  var stateTenantId = 'in'
-  var gmaps_api_key = 'AIzaSyAQOd09-vjmk1sXFb_ZQYDz2nlfhXq7Wf8'
-  var getConfig = function(key) {
-      if(key === 'STATE_LEVEL_TENANT_ID'){
-          return stateTenantId;
-      } 
-      else if(key === 'GMAPS_API_KEY'){
-        return gmaps_api_key;
-      }
+"use strict";
+
+var globalConfigs = function () {
+  var stateTenantId = 'in';
+  var gmaps_api_key = 'AIzaSyAQOd09-vjmk1sXFb_ZQYDz2nlfhXq7Wf8';
+
+  var getConfig = function getConfig(key) {
+    if (key === 'STATE_LEVEL_TENANT_ID') {
+      return stateTenantId;
+    } else if (key === 'GMAPS_API_KEY') {
+      return gmaps_api_key;
+    }
   };
+
   return {
-      getConfig
+    getConfig: getConfig
   };
-}());
+}();
